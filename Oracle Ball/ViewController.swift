c62f12dbd9af103246c7de78102fb323478f903d
FrancaTM <francatm@outlook.com>
2020-09-29 11:51:32 -0300

    make ball image random

--- Oracle Ball/ViewController.swift
+++ Oracle Ball/ViewController.swift
@@ -8,19 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballImageArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")];
 
     @IBOutlet weak var ballImageView: UIImageView!
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        ballImageView.image = #imageLiteral(resourceName: "ball3")
+        ballImageView.image = ballImageArray[Int.random(in: 0...4)]
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
 
